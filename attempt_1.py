7cab0c7e377e96b7c8af904fe858dffad1c2ade3
shanemarvinmay <shanemarvinmay@gmail.com>
2020-02-20 14:28:56 -0600

    untested order_ls done

---
+++ attempt_1.py
@@ -0,0 +1,29 @@
+def read_file(file_name):
+    f = open(file_name, 'r')
+
+    f.close()
+
+def order_ls(ignore_B, ignore_L, D_remaining, Ls, B_to_pts):
+    output = []
+    # ordering the ls by the most pts/D 
+    while len(ignore_L) < Ls:
+        # finding L with most pts/D 
+        idx = -1
+        most = -1
+        most_numB = -1
+        for L in Ls:
+            if L in ignore_L or L['signup'] >= D_remaining:
+                continue
+            pts, numB = calc_pts_D( Ls[L] )
+            if pts > most:
+                idx = L
+                most = pts
+                most_numB = numB
+        D_remaining -= Ls[idx]['signup']
+        # adding all B to ignore_B
+        for i in range(most_numB):
+            ignore_B.add(Ls[idx]['books'][i])
+        ignore_L.add(idx)
+        output.append(idx)
+    return output
+
